6d586eb99a084173212adac29b4559a6e64c0ad9
mohit kumar <mksujanian2683@gmail.com>
2018-04-04 21:30:21 +0530

    basic functionality

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'NfvkFeDfn0Yr7JgvBtHxPRpWE',
+	consumer_secret: 'UrffgpkL9f4Cb3H6rstQSrk1ghlirwoDmvwHgWElSyUJ5Bcrpf',
+	access_token_key: '914101737178284032-XquNvh53putzjaOD9vHwyHlNqXFJb9X',
+	access_token_secret: 'hJEJ12csXnXV2KbKkXjoC59Hcj2MYhdmo6NSpYtZUrXLy'
+}
